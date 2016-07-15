21e912673df9bba45a506f62c2da333fcdcf47b3
Sorrell123 <multibrunio@gmail.com>
2016-07-15 09:42:26 +0200

    First Worpdress theme

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pierwszyWordPress</title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+	<div id="wraper"></div>
+	<div id="header">
+		<h1>HEADER</h1>
+	</div>
+</body>
+</html>
